471df489144cdd013051390213b3e1260999dbc1
massu-159 <magjtwjp@gmail.com>
2023-09-26 07:39:05 +0900

    create base layouts

---
+++ app/components/navigation/Navigation.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Link from 'next/link'
+
+const Navigation = () => { 
+  return (
+    <header className='shadow-lg shadow-gray-100'>
+      <div className="container mx-auto flex max-w-screen-sm items-center justify-between px-1 py-5">
+        <Link href='/' className='cursor-pointer text-xl font-bold'>
+          NextAuthTemplate
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Navigation
