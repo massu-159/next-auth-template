c517d839fac967805ef81a9e4fdeed969128df7f
massu-159 <magjtwjp@gmail.com>
2023-09-27 08:17:04 +0900

    add auth context

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+type AuthContextProps = {
+  children: React.ReactNode
+}
+
+const AuthContext = ({ children }: AuthContextProps) => {
+  return <SessionProvider>{children}</SessionProvider>
+}
+
+export default AuthContext
