e2fbc3bd4bf55a97e1dd958cb3c8a8ff66f36e79
massu-159 <magjtwjp@gmail.com>
2023-09-27 21:53:37 +0900

    add types & toaster

---
+++ app/context/ToastContext.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+// https://react-hot-toast.com/docs
+import { Toaster } from 'react-hot-toast'
+
+const ToasterContext = () => {
+  return <Toaster />
+}
+
+export default ToasterContext
