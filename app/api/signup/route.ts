82246716190d5abe02d6c0a783b9fd4aef8db69a
massu-159 <magjtwjp@gmail.com>
2023-09-27 22:39:02 +0900

    create signup api

---
+++ app/api/signup/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server"
+import bcrypt from "bcrypt"
+import prisma from '@/app/lib/prisma'
+
+// サインアップ　POST /api/signup
+export async function POST(request: Request) {
+  try {
+    // リクエストボディの取得
+    const body = await request.json()
+    const { name, email, password } = body
+
+    // パスワードのハッシュ化
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    // ユーザーの作成
+    const response = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    })
+    
+    return NextResponse.json(response)
+  }
+catch (error) {
+    console.log(error)
+    return new NextResponse('Error', { status: 500 })
+}
+}
