3cdd7a6c11b6e8417d28c116f3ddebd2f0decd75
massu-159 <magjtwjp@gmail.com>
2023-09-27 23:08:47 +0900

    add google auth

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['1h3.googleusercontent.com', 'res.cloudinary.com'],
+  },
+}
 
 module.exports = nextConfig
